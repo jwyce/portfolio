ae77972c36b7eca6e76bdc3aea131e135de8845b
jwyce <wycejared@gmail.com>
2021-11-15 22:04:51 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,183 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import dayjs from 'dayjs';
+import React, { useEffect, useState } from 'react';
+import koiImage from './assets/koi.png';
+import rubikImage from './assets/rubik.svg';
+import portfolioImage from './assets/portfolio.svg';
+
+const YEAR_MILLIS = 1000 * 60 * 60 * 24 * 365;
+enum Skill {
+	NODE = 'node',
+	REACT = 'react',
+	GRAPHQL = 'graphql',
+	OTHER = 'other',
+	NONE = 'none',
 }
 
-export default App;
+export const App: React.FC = () => {
+	const [age, setAge] = useState(
+		dayjs().diff(dayjs('1997-06-17', 'year', true)) / YEAR_MILLIS
+	);
+	const [skill, setSkill] = useState(Skill.NONE);
+
+	const refreshAge = () => {
+		setAge(dayjs().diff(dayjs('1997-06-17', 'year', true)) / YEAR_MILLIS);
+	};
+
+	useEffect(() => {
+		const timerId = setInterval(refreshAge, 100);
+		return function cleanup() {
+			clearInterval(timerId);
+		};
+	}, []);
+
+	let skillSection = <></>;
+	let border = 'transparent';
+	switch (skill) {
+		case Skill.NODE:
+			skillSection = <h3>nodejs</h3>;
+			border = '#90c53f';
+			break;
+		case Skill.REACT:
+			skillSection = <h3>react</h3>;
+			border = '#5ec9f8';
+			break;
+		case Skill.GRAPHQL:
+			skillSection = <h3>graphql</h3>;
+			border = '#e535ab';
+			break;
+		case Skill.OTHER:
+			skillSection = <h3>python, java, c#</h3>;
+			border = '#9158f1';
+			break;
+	}
+
+	return (
+		<>
+			<div className="main-container">
+				<h1 style={{ marginBottom: 0 }}>
+					{'{'} Jared Wyce {'}'}
+				</h1>
+				<h2 style={{ marginTop: 0, marginBottom: 0 }}>
+					I'm a {age.toString().substr(0, 12)} year-old developer
+				</h2>
+				<h2 style={{ marginTop: 0 }}>
+					who uses{' '}
+					<span
+						className="d"
+						style={{ color: '#90c53f' }}
+						onClick={() =>
+							skill === Skill.NODE ? setSkill(Skill.NONE) : setSkill(Skill.NODE)
+						}
+					>
+						Node.JS
+					</span>
+					,{' '}
+					<span
+						className="d"
+						style={{ color: '#5ec9f8' }}
+						onClick={() =>
+							skill === Skill.REACT
+								? setSkill(Skill.NONE)
+								: setSkill(Skill.REACT)
+						}
+					>
+						React
+					</span>
+					,{' '}
+					<span
+						className="d"
+						style={{ color: '#e535ab' }}
+						onClick={() =>
+							skill === Skill.GRAPHQL
+								? setSkill(Skill.NONE)
+								: setSkill(Skill.GRAPHQL)
+						}
+					>
+						GraphQL
+					</span>
+					, and{' '}
+					<span
+						className="d"
+						style={{ color: '#9158f1' }}
+						onClick={() =>
+							skill === Skill.OTHER
+								? setSkill(Skill.NONE)
+								: setSkill(Skill.OTHER)
+						}
+					>
+						others
+					</span>
+					.
+				</h2>
+
+				<h2 style={{ marginBottom: 0 }}>
+					Check out my <a href="https://github.com/jwyce">GitHub</a> and{' '}
+					<a href="https://www.linkedin.com/in/jaredwyce/">LinkedIn</a>
+				</h2>
+				<h2 style={{ marginTop: 0, marginBottom: '2em' }}>
+					Business inquiry? <a href="mailto:wycejared@gmail.com">Email me!</a>
+				</h2>
+			</div>
+			<div className="projects-container">
+				<div className="project">
+					<div
+						className="project-image"
+						onClick={() => window.open('https://gungi.io')}
+						style={{
+							cursor: 'pointer',
+							backgroundImage: `url("https://raw.githubusercontent.com/jwyce/gungi.io/master/client/public/images/apple-touch-icon.png")`,
+						}}
+					/>
+					<span className="project-title">Gungi.io</span>
+				</div>
+				<div className="project">
+					<div
+						className="project-image"
+						onClick={() => window.open('https://github.com/jwyce/koi-anime')}
+						style={{
+							cursor: 'pointer',
+							backgroundImage: `url(${koiImage})`,
+						}}
+					/>
+					<span className="project-title">Koi Anime (WIP)</span>
+				</div>
+				<div className="project">
+					<div
+						className="project-image"
+						onClick={() =>
+							window.open('https://github.com/jwyce/2x2x2-Rubik-Solver')
+						}
+						style={{
+							cursor: 'pointer',
+							backgroundImage: `url(${rubikImage})`,
+							backgroundColor: '#3c3d5c',
+						}}
+					/>
+					<span className="project-title">2x2x2 Rubik Solver</span>
+				</div>
+				<div className="project">
+					<div
+						className="project-image"
+						onClick={() =>
+							window.open('https://github.com/jwyce/2x2x2-Rubik-Solver')
+						}
+						style={{
+							cursor: 'pointer',
+							backgroundImage: `url(${portfolioImage})`,
+							backgroundColor: '#3c3836',
+						}}
+					/>
+					<span className="project-title">Portfolio</span>
+				</div>
+			</div>
+			<div
+				className="skill-container"
+				style={{ border: `solid 3px ${border}` }}
+			>
+				{skillSection}
+			</div>
+		</>
+	);
+};
