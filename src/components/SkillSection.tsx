51b1b11ab9d3a704a1c69ffbab4976c0a7494d68
Jared Wyce <wycejared@gmail.com>
2022-04-30 16:26:00 -0400

    💄 Update skill icons

--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -64,12 +64,12 @@ export const SkillSection: React.FC<SkillProps> = ({ skill }) => {
 					/>
 					<TechStackItem
 						href="https://developer.mozilla.org/en-US/docs/Web/CSS"
-						url="https://github.com/vscode-icons/vscode-icons/raw/master/icons/file_type_css.svg"
+						url="https://raw.githubusercontent.com/BeardedBear/bearded-icons/87232606252d2dba1dac3c86583c14d8c09b5574/icons/css.svg"
 						name="CSS"
 					/>
 					<TechStackItem
 						href="https://www.docker.com/"
-						url="https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/eddd21641e769b1d4d8974834fb75166038b2e52/icons/docker.svg"
+						url="https://raw.githubusercontent.com/BeardedBear/bearded-icons/87232606252d2dba1dac3c86583c14d8c09b5574/icons/docker.svg"
 						name="Docker"
 					/>
 					<TechStackItem
@@ -94,12 +94,12 @@ export const SkillSection: React.FC<SkillProps> = ({ skill }) => {
 				<motion.h3 className="tech-stack-container">
 					<TechStackItem
 						href="https://elixirschool.com/en"
-						url="https://github.com/vscode-icons/vscode-icons/raw/master/icons/file_type_elixir.svg"
+						url="https://raw.githubusercontent.com/BeardedBear/bearded-icons/87232606252d2dba1dac3c86583c14d8c09b5574/icons/elixir.svg"
 						name="Elixir"
 					/>
 					<TechStackItem
 						href="https://doc.rust-lang.org/book/title-page.html"
-						url="https://rustacean.net/assets/rustacean-orig-noshadow.svg"
+						url="https://raw.githubusercontent.com/BeardedBear/bearded-icons/87232606252d2dba1dac3c86583c14d8c09b5574/icons/rust.svg"
 						name="Rust"
 					/>
 					<TechStackItem
@@ -118,9 +118,9 @@ export const SkillSection: React.FC<SkillProps> = ({ skill }) => {
 						name="Solid"
 					/>
 					<TechStackItem
-						href="https://www.cypress.io/"
-						url="https://github.com/vscode-icons/vscode-icons/raw/master/icons/file_type_cypress.svg"
-						name="Cypress"
+						href="https://vitest.dev/"
+						url="https://vitest.dev/logo.svg"
+						name="Vitest"
 					/>
 					<TechStackItem
 						href="https://vitejs.dev/"
@@ -138,14 +138,9 @@ export const SkillSection: React.FC<SkillProps> = ({ skill }) => {
 						name="TypeScript"
 					/>
 					<TechStackItem
-						href="https://nodejs.org/en/docs/"
-						url="https://github.com/vscode-icons/vscode-icons/raw/master/icons/file_type_node.svg"
-						name="Node"
-					/>
-					<TechStackItem
-						href="https://beta.reactjs.org/"
+						href="https://nextjs.org/"
 						url="https://github.com/vscode-icons/vscode-icons/raw/master/icons/file_type_reactjs.svg"
-						name="React"
+						name="Next.js"
 					/>
 					<TechStackItem
 						href="https://trpc.io/"
@@ -154,53 +149,23 @@ export const SkillSection: React.FC<SkillProps> = ({ skill }) => {
 					/>
 					<TechStackItem
 						href="https://tailwindcss.com/"
-						url="https://github.com/vscode-icons/vscode-icons/raw/master/icons/file_type_tailwind.svg"
+						url="https://raw.githubusercontent.com/BeardedBear/bearded-icons/87232606252d2dba1dac3c86583c14d8c09b5574/icons/tailwind.svg"
 						name="Tailwind CSS"
 					/>
 					<TechStackItem
-						href="https://nextjs.org/"
-						url="https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/eddd21641e769b1d4d8974834fb75166038b2e52/icons/next.svg"
-						name="Next"
+						href="https://vercel.com/home"
+						url="https://raw.githubusercontent.com/BeardedBear/bearded-icons/87232606252d2dba1dac3c86583c14d8c09b5574/icons/vercel.svg"
+						name="Vercel"
 					/>
 					<TechStackItem
 						href="https://www.prisma.io/"
-						url="https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/eddd21641e769b1d4d8974834fb75166038b2e52/icons/prisma.svg"
+						url="https://raw.githubusercontent.com/BeardedBear/bearded-icons/87232606252d2dba1dac3c86583c14d8c09b5574/icons/prisma.svg"
 						name="Prisma"
 					/>
 					<TechStackItem
 						href="https://planetscale.com/"
 						url="/pscale.svg"
 						name="PlanetScale"
-					/>
-					<TechStackItem
-						href="https://next-auth.js.org/"
-						url="https://next-auth.js.org/img/logo/logo-sm.png"
-						name="NextAuth"
-					/>
-					<TechStackItem
-						href="https://react-query.tanstack.com/"
-						url="https://camo.githubusercontent.com/88a0a0e7271c20473dcea78f99c6e8c419ebcbe5e1a62229b2b3fae336825175/68747470733a2f2f72656163742d71756572792e74616e737461636b2e636f6d2f5f6e6578742f7374617469632f696d616765732f656d626c656d2d6c696768742d36323830383036363066646462333537383766663663373765393763613433652e737667"
-						name="React Query"
-					/>
-					<TechStackItem
-						href="https://pusher.com/"
-						url="https://pusher.com/favicon-196x196.png"
-						name="Pusher"
-					/>
-					<TechStackItem
-						href="https://vercel.com/home"
-						url="https://github.com/vscode-icons/vscode-icons/raw/master/icons/file_type_zeit.svg"
-						name="Vercel"
-					/>
-					<TechStackItem
-						href="https://turborepo.org/"
-						url="https://github.com/vscode-icons/vscode-icons/raw/master/icons/file_type_turbo.svg"
-						name="Turborepo"
-					/>
-					<TechStackItem
-						href="https://github.com/home"
-						url="https://github.com/fluidicon.png"
-						name="GitHub"
 					/>
 				</motion.div>
 			);
